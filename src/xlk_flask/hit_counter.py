4cdd9d5dcc244aa6307ac070310ac899eefa3b24
mxwll <mxwllndrsn@gmail.com>
2024-01-23 23:33:36 -0800

    flask single route

--- src/xlk_flask/hit_counter.py
+++ src/xlk_flask/hit_counter.py
@@ -15,33 +15,33 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
-@app.route('/xlk-user-counter-API', methods=['POST'])
-def increment():
-    username = request.json.get('username')
-    try:
-        response = table.update_item(
-            Key = {'Username': username},
-            UpdateExpression = 'ADD Counter :inc',
-            ExpressionAttributeValues = {':inc': 1},
-            ReturnValues = 'UPDATED_NEW'
-        )
-        return jsonify(response['Attributes']), 200
-    except ClientError as e:
-        return jsonify({'error': e.response['Error']['Message']}), 500
-    
+@app.route('/', methods=['POST'])
+def index():
+    ## increment count
+    if request.method == 'POST':
+        username = request.json.get('username')
+        try:
+            response = table.update_item(
+                Key = {'Username': username},
+                UpdateExpression = 'ADD Counter :inc',
+                ExpressionAttributeValues = {':inc': 1},
+                ReturnValues = 'UPDATED_NEW'
+            )
+            return jsonify(response['Attributes']), 200
+        except ClientError as e:
+            return jsonify({'error': e.response['Error']['Message']}), 500
+    ## get count
+    elif request.method == 'GET':
+        username = request.args.get('username')
+        try: 
+            response = table.get_item(Key = {'Username': username})
+            if 'Item' in response:
+                return jsonify(response['Item']), 200
+            else: 
+                return jsonify({'error': 'User not found'}), 404
+        except ClientError as e:
+            return jsonify({'error': e.response['Error']['Message']}), 500 
 
-@app.route('/xlk-user-counter-API', methods=['GET'])
-def get_count():
-    username = request.args.get('username')
-    try: 
-        response = table.get_item(Key = {'Username': username})
-        if 'Item' in response:
-            return jsonify(response['Item']), 200
-        else: 
-            return jsonify({'error': 'User not found'}), 404
-    except ClientError as e:
-        return jsonify({'error': e.response['Error']['Message']}), 500 
-    
 
 lambda_handler = make_lambda_handler(app)
 
