b315a50602c2409b6fd8d54268406c43a32754b2
mxwll <mxwllndrsn@gmail.com>
2024-01-26 23:09:45 -0800

    flask: split routes, dyanomo table update

--- src/xlk_flask/hit_counter.py
+++ src/xlk_flask/hit_counter.py
@@ -11,53 +11,51 @@ from boto3.dynamodb.conditions import Key
 app = Flask(__name__)
 
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('xlk_user_button_hit_counter')
+table = dynamodb.Table('xlk_user_counter')
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-@app.route('/xlk-user-button-counter-increment', methods=['POST', 'GET'])
-def index():
-    ## increment count
-    if request.method == 'POST':
-        username = request.json.get('username')
 
-        if not username: 
-            return jsonify({'error': 'Username parameter is required'}), 400  
-        
-        try:
-            response = table.update_item(
-                Key = {'Username': username},
-                UpdateExpression = 'ADD total_hit :inc',
-                ExpressionAttributeValues = {':inc': 1},
-                ReturnValues = 'UPDATED_NEW'
-            )
-            return jsonify(response['Attributes']), 200
-        except ClientError as e:
-            return jsonify({'error': e.response['Error']['Message']}), 500
-        
-    ## get count
-    elif request.method == 'GET':
-        username = request.args.get('username')    
+@app.route('/xlk-user-button-counter-increment', methods=['POST'])
+def increment():
+    username = request.json.get('username')
 
-        if not username:  
-            return jsonify({'error': 'Username parameter is required'}), 400
-        
-        try: 
-            response = table.query(
-                IndexName="xlkUserButtonHitCounter-username-index",
-                KeyConditionExpression=Key('Username').eq('admin')
-            )
-            if 'Item' in response:
-                return jsonify(response['Item']), 200
-            else: 
-                return jsonify({'error': 'User not found'}), 404
-        except ClientError as e:
-            return jsonify({'error': e.response['Error']['Message']}), 500 
+    if not username: 
+        return jsonify({'error': 'username parameter is required'}), 400  
+
+    try:
+        response = table.update_item(
+            Key = {'username': username},
+            UpdateExpression = 'ADD pressCount :inc',
+            ExpressionAttributeValues = {':inc': 1},
+            ReturnValues = 'UPDATED_NEW'
+        )
+        return jsonify(response['Attributes']), 200
+    except ClientError as e:
+        return jsonify({'error': e.response['Error']['Message']}), 500
+    
+
+@app.route('/xlk-user-button-counter-increment', methods=['GET'])
+def get_count():
+    username = request.args.get('username')
+
+    if not username: 
+        return jsonify({'error': 'username parameter is required'}), 400  
+
+    try: 
+        response = table.get_item(Key = {'username': username})
+        if 'Item' in response:
+            return jsonify(response['Item']), 200
+        else: 
+            return jsonify({'error': 'User not found'}), 404
+    except ClientError as e:
+        return jsonify({'error': e.response['Error']['Message']}), 500 
+
 
 @app.errorhandler(404)
 def invalid_route(e):
-    return "oh god oh fuck"
+    return "404 error - route not found"
 
 def lambda_handler(event, context):
     logger.info(json.dumps(event))
